3722ff45896bdc82938b8411b96531d85271069b
msemen <mitjasemen19@gmail.com>
2025-03-03 01:34:39 +0100

    commit Orleans and testProject

---
+++ Orleans/Grains/BreachGrain.cs
@@ -0,0 +1,40 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+public class BreachGrain : Grain, IBreachGrain
+{
+    private bool _isBreached;  // Represents the breach status of the email.
+
+    // On activation (first time), we would load the status from Blob Storage
+    public override async Task OnActivateAsync(CancellationToken token)
+    {
+        // Retrieve the breach status from Azure Blob Storage (simulated in this case)
+        var status = await GetStatusFromBlobStorage();
+        _isBreached = status;
+    }
+
+    public Task<bool> IsBreached()
+    {
+        return Task.FromResult(_isBreached);
+    }
+
+    public async Task SetBreachedStatus(bool status)
+    {
+        _isBreached = status;
+        await SaveStatusToBlobStorage(status);  // Simulate saving to Blob Storage
+    }
+
+    // Simulated method to retrieve the status from Blob Storage (replace with actual Azure Blob Storage logic)
+    private Task<bool> GetStatusFromBlobStorage()
+    {
+        // For now, returning a default value (this can be replaced with actual Azure interaction)
+        return Task.FromResult(false);  // Assume email is not breached for now
+    }
+
+    // Simulated method to save the status to Blob Storage
+    private Task SaveStatusToBlobStorage(bool status)
+    {
+        // For now, we don't do anything in this simulation
+        return Task.CompletedTask;
+    }
+}
---
+++ Orleans/Grains/IBreachGrain.cs
@@ -0,0 +1,8 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+public interface IBreachGrain : IGrainWithStringKey
+{
+    Task<bool> IsBreached();
+    Task SetBreachedStatus(bool status);
+}
